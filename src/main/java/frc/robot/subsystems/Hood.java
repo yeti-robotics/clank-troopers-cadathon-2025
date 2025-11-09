3a41c9306d0696093ff189b00330a693f2e20152
DL <thedanyaliu@gmail.com>
2025-11-09 15:58:05 -0500

    setup all subsystems and made drivetrain

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -2,4 +2,5 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Control extends SubsystemBase {}
+public class Hood extends SubsystemBase {
+}
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,3 +1,5 @@
 package frc.robot.subsystems;
 
-public class Shooter {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {}
