5207d4928aa7109df1c1cb38171246e164d39f58
anishb77 <anishboddu7@gmail.com>
2025-11-15 19:21:58 -0500

    added temporary arm io

--- src/main/java/frc/robot/subsystems/arm/ArmConfig.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConfig.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.arm;
 
 public class ArmConfig {
-    public static final int MAIN_ARM_MOTOR_ID = 15;
-    public static final int FOLLOW_ARM_MOTOR_ID = 16;
+    static final int MAIN_ARM_MOTOR_ID = 15;
+    static final int FOLLOW_ARM_MOTOR_ID = 16;
 }
--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -1,4 +1,17 @@
 package frc.robot.subsystems.arm;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ArmIO {
+    @AutoLog
+    public class ArmIOInputs {
+        public double positionDeg = 0.0;
+        public double velocityDegPerSec = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+    }
+
+    public default void updateInputs(ArmIOInputs inputs) {}
+
+    public default void setPower(double power) {}
 }
