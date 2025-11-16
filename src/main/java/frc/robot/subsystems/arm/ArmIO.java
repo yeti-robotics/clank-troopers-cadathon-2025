db5f19b8c03d7a899eba89b96ba35978ae835ea3
anishb77 <anishboddu7@gmail.com>
2025-11-15 20:31:59 -0500

    updated arm io for position variables

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,10 +5,15 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
     @AutoLog
     public class ArmIOInputs {
-        public double positionDeg = 0.0;
+        public double positionDegCurrent = 0.0;
+        public double positionDegTarget = 0.0;
         public double velocityDegPerSec = 0.0;
         public double appliedVolts = 0.0;
         public double currentAmps = 0.0;
+
+        // for target position and current position, 0 is within bumper and 1 is read to intake
+        public int targetPosition = 0;
+        public int currentPosition = 1;
     }
 
     public default void updateInputs(ArmIOInputs inputs) {}
