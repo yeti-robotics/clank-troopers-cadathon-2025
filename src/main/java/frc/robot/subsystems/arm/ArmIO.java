fd78fe8c538d5a6fee4a2fdac421638efe06f0c5
anishb77 <anishboddu7@gmail.com>
2025-11-15 22:24:44 -0500

    altered armio and created io sim prototype

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -5,18 +5,16 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ArmIO {
     @AutoLog
     public class ArmIOInputs {
-        public double positionDegCurrent = 0.0;
-        public double positionDegTarget = 0.0;
+        public double positionDegCurrentMain = 0.0;
+        public double positionDegTargetMain = 0.0;
+        public double positionDegCurrentFollow = 0.0;
+        public double positionDegTargetFollow = 0.0;
         public double velocityDegPerSec = 0.0;
         public double appliedVolts = 0.0;
         public double currentAmps = 0.0;
-
-        // for target position and current position, 0 is within bumper and 1 is read to intake
-        public int targetPosition = 0;
-        public int currentPosition = 1;
     }
 
     public default void updateInputs(ArmIOInputs inputs) {}
 
-    public default void setPower(double power) {}
+    public default void moveToPosition(double position) {}
 }
